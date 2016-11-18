abf94e3091e54f064f62293bc8e693f420d05088
rmquapp <rmquapp@gmail.com>
2016-11-18 03:03:36 -0700

    Designed sign up page, updated theme, fixed activity flow

--- app/src/main/java/com/accipio/tutorme/SetupActivity.java
+++ app/src/main/java/com/accipio/tutorme/SetupActivity.java
@@ -1,11 +1,20 @@
 package com.accipio.tutorme;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.preference.PreferenceManager;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
@@ -13,8 +22,12 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class SetupActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class SetupActivity extends AppCompatActivity implements View.OnClickListener, OnNavigationItemSelectedListener {
 
     private TextView info;
 
@@ -22,10 +35,29 @@ public class SetupActivity extends AppCompatActivity implements View.OnClickList
 
     private int[] ids = {R.id.email, R.id.toLearn, R.id.toTeach, R.id.description, R.id.rate};
 
+    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+
         setContentView(R.layout.activity_setup);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_setup_layout);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(false);
+        View mActionBarView = getLayoutInflater().inflate(R.layout.action_bar, null);
+        actionBar.setCustomView(mActionBarView);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+
+        int resourceId = getResources().getIdentifier("actionbar_bg", "drawable", getPackageName());
+        getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(resourceId));
 
         info = (TextView)findViewById(R.id.info);
         findViewById(R.id.go).setOnClickListener(this);
@@ -37,6 +69,8 @@ public class SetupActivity extends AppCompatActivity implements View.OnClickList
         restoreInfo();
 
         setupBubbleTextViews();
+
+        setupNavigationDrawer();
     }
 
     private void showName() {
@@ -91,6 +125,71 @@ public class SetupActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        goToSelection(id);
+        return true;
+    }
+
+    private void goToSelection(int id) {
+        Intent intent = null;
+        switch (id) {
+            case R.id.drawer_home:
+                intent = new Intent(this, BrowseActivity.class);
+                break;
+            case R.id.drawer_settings:
+                // Browse activity, no page change
+                toggleMenu(getCurrentFocus());
+                return;
+            case R.id.drawer_messages:
+                // TODO: replace with messaging activity if we get to that point
+                return;
+            case R.id.drawer_about:
+                // TODO: replace with about page
+                return;
+            case R.id.drawer_policy:
+                // TODO: replace with privacy policy page
+                return;
+            default:
+                break;
+        }
+        drawer.closeDrawer(Gravity.LEFT, false);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    public void toggleMenu(View view) {
+        if (drawer.isDrawerOpen(Gravity.LEFT)) {
+            drawer.closeDrawer(Gravity.LEFT);
+        }
+        else {
+            drawer.openDrawer(Gravity.LEFT);
+        }
+    }
+
+    private void setupNavigationDrawer() {
+        NavigationView navView = (NavigationView) findViewById(R.id.menu);
+        navView.setNavigationItemSelectedListener(this);
+
+        View hView =  navView.getHeaderView(0);
+
+        String fname = ((TutorMeApplication) SetupActivity.this.getApplication()).getFirstName();
+        String lname = ((TutorMeApplication) SetupActivity.this.getApplication()).getLastName();
+        TextView name = (TextView)hView.findViewById(R.id.header_name);
+        name.setText(String.format("%s %s", fname, lname));
+
+        Bitmap picture = ((TutorMeApplication) SetupActivity.this.getApplication()).getImage();
+        CircleImageView image = (CircleImageView)hView.findViewById(R.id.header_image);
+        image.setImageBitmap(picture);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String email = prefs.getString("email", "");
+        TextView emailView = (TextView)hView.findViewById(R.id.header_email);
+        emailView.setText(email);
+    }
+
     private void restoreInfo() {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
@@ -122,5 +221,10 @@ public class SetupActivity extends AppCompatActivity implements View.OnClickList
         editor.putBoolean("isTutor", isTutor);
 
         editor.apply();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
