5421a9afa59b1c85bc21b0ec626e80575efa7014
Mike <michealfriesen@gmail.com>
2016-11-22 00:31:15 -0700

    THE ANIMATION HAS BEEN BUILT

--- app/src/main/java/com/accipio/tutorme/MainMenu.java
+++ app/src/main/java/com/accipio/tutorme/MainMenu.java
@@ -5,17 +5,20 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
-public class MainMenu extends Activity {
+public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         // Begin the animation on the menu circle
         Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.spin_2_win);
